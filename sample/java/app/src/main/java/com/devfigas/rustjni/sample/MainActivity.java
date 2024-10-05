f2f522dd4338ad93a4b58255a946af69bf9245c1
andre.figueiredo <andre.figueiredo@miniclip.com>
2024-10-05 19:02:57 +0100

    Split samples in kotlin and java

---
+++ sample/java/app/src/main/java/com/devfigas/rustjni/sample/MainActivity.java
@@ -0,0 +1,25 @@
+package com.devfigas.rustjni.sample;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    //<RustJNI>
+    // auto-generated code
+
+    
+    private static native String someMethod(String param0);
+    
+
+    static { System.loadLibrary("my_rust_lib"); }
+
+    //</RustJNI>
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ sample/java/app/src/main/rust/src/rust_jni.rs
@@ -0,0 +1,32 @@
+use jni::JNIEnv;
+use jni::objects::JClass;
+//<RustJNI>
+// primitive imports
+use jni::sys::{jint, jstring};
+//</RustJNI>        
+#[no_mangle]
+pub extern "C" fn Java_com_devfigas_rustjni_sample_MainActivity_someMethod(
+    env: JNIEnv,
+    _class: JClass,
+    param0: jint
+) -> jstring {
+    println!("Parameters: {:?}", (param0));
+
+    let output = r#"Rust Method: someMethod"#;
+env.new_string(output)
+    .expect("Couldn't create Java string!")
+    .into_inner()
+}        
+#[no_mangle]
+pub extern "C" fn Java_com_devfigas_rustjni_sample_MainActivity_someMethod(
+    env: JNIEnv,
+    _class: JClass,
+    param0: jstring
+) -> jstring {
+    println!("Parameters: {:?}", (param0));
+
+    let output = r#"Rust Method: someMethod"#;
+env.new_string(output)
+    .expect("Couldn't create Java string!")
+    .into_inner()
+}
