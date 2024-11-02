5bdae6e3edc856ff444f51a1a54293c2b3b605e2
Megadash452 <64887017+Megadash452@users.noreply.github.com>
2024-11-02 13:45:33 -0400

    Fixed the libName that the jni Class tries to load being empty (#14)
    * Updated gradle versions
    
    * Moved data in pluginBundle to gradlePlugin
    
    * Fixed jni host Class loading empty libName
    
    * Update gradle version for gradle-plugin-test
    
    * Release 0.0.19
    
    * Bump sample plugin dependency
    
    Release 0.0.16
    
    * Update pluggin references on readme
    
    ---------

--- sample/java/app/src/main/java/com/devfigas/rustjni/sample/MainActivity.java
+++ sample/java/app/src/main/java/com/devfigas/rustjni/sample/MainActivity.java
@@ -6,6 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    //<RustJNI>
+    // auto-generated code
+
+    private static native String sayHello();
+
+    static { System.loadLibrary("my_rust_lib"); }
+
+    //</RustJNI>
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
--- sample/kotlin/app/src/main/java/com/devfigas/rustjni/sample/MainActivity.kt
+++ sample/kotlin/app/src/main/java/com/devfigas/rustjni/sample/MainActivity.kt
@@ -6,6 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
+    //<RustJNI>
+    // auto-generated code
+
+    private external fun sayHello(): String
+
+    init { System.loadLibrary("my_rust_lib") }
+
+    //</RustJNI>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
