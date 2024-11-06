2bcbc0fa81d6c32f3253e2d0e23a040802d2602c
Andre <andre.ce89@gmail.com>
2024-11-06 17:41:27 +0000

    Add game sample (#22)

---
+++ sample/game/app/build.gradle.kts
@@ -0,0 +1,64 @@
+import io.github.andrefigas.rustjni.reflection.Visibility
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    id("io.github.andrefigas.rustjni") version "0.0.22"
+}
+
+rustJni{
+    jniHost = "com.devfigas.gamesample.NativeLib"
+    jniMethodsVisibility = Visibility.PUBLIC
+    rustPath = "./app/src/main/rust"
+    ndkVersion = "27.1.12297006"
+    architectures {
+        armv7_linux_androideabi("armv7a-linux-androideabi21-clang")
+        aarch64_linux_android("aarch64-linux-android21-clang")
+        i686_linux_android("i686-linux-android21-clang")
+        x86_64_linux_android("x86_64-linux-android21-clang")
+    }
+}
+
+android {
+    namespace = "com.devfigas.gamesample"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.devfigas.gamesample"
+        minSdk = 24
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
